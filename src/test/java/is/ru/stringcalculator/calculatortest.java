08c101d94e51f7b80d037c3d1b852dfb2a9162ca
steindors13 <steindors13@ru.is>
2016-10-10 15:34:53 +0000

    missing par

--- src/test/java/is/ru/stringcalculator/calculatortest.java
+++ src/test/java/is/ru/stringcalculator/calculatortest.java
@@ -1,3 +1,16 @@
-package is.ru.stringcalculator
+package is.ru.stringcalculator;
 
 import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class calculatortest{
+
+	public static void main(String args[]) {
+		org.junit.runner.JUnitCore.main("is.ru.stringcalculator.calculatortest");
+	}
+
+	@Test
+	/*public void test() {
+		assertEquals(0, calculator.add(""));
+	}*/
+}
