f112e6366c8a30945450529ebe75f82652bdf7df
steindors13 <steindors13@ru.is>
2016-10-11 16:26:31 +0000

    finally I can start the project

--- src/test/java/is/ru/stringcalculator/calculatortest.java
+++ src/test/java/is/ru/stringcalculator/calculatortest.java
@@ -9,20 +9,6 @@ public class CalculatorTest {
       org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
     }
 
-	@Test
-	public void testEmptyString() {
-		assertEquals(0, Calculator.add(""));
-	}
-
-	@Test
-	public void testOneNumber() {
-		assertEquals(1, Calculator.add("1"));
-	}
-
-	@Test
-	public void testTwoNumbers() {
-		assertEquals(3, Calculator.add("1,2"));
-	}	
 
 	@Test
     public void testMultipleNumbers(){
